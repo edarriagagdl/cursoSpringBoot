e458bc560476ddbff6509ea7f455c85324bc4196
edarriagagdl <edarriaga@gmail.com>
2024-09-22 16:30:13 -0600

    Mi Primer commit

---
+++ src/main/java/mx/dev/edarriaga/plantapp2/Plantapp2Application.java
@@ -0,0 +1,14 @@
+package mx.dev.edarriaga.plantapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plantapp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Plantapp2Application.class, args);
+        System.out.println ("hola");
+    }
+
+}
---
+++ src/test/java/mx/dev/edarriaga/plantapp2/Plantapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.dev.edarriaga.plantapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plantapp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
